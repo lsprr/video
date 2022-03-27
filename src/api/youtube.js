8f76d0a81d3402b00dcdb4dc70019985616cb275
lsprr <16653744+lsprr@users.noreply.github.com>
2022-03-27 13:53:04 -0400

    Add custom environment variable

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const KEY = YOUR_SECRET_KEY;
+const KEY = process.env.REACT_APP_YOUTUBE_API;
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
